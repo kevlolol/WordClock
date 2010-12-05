20b425ed4cdbce0455d5e31d5e3f1043680a43a2
kevlolol <kevin@kevlolol.com>
2010-12-05 03:48:11 +0000

    Updated class for basic features.

--- loop_cpp/include/arduino.h
+++ loop_cpp/include/arduino.h
@@ -3,33 +3,91 @@
 // AUTHORS: Kevin Buckner
 //   VERSION
 //     - 0.1 Initial Creation
-//-----------------------------------------------------------------------------
-///-------------------------------------
+///----------------------------------------------------------------------------
 /// INCLUDE GUARD
-///-------------------------------------
+///----------------------------------------------------------------------------
 #ifndef _ARDUINO_H_ 
 #define _ARDUINO_H_
-///-------------------------------------
+///----------------------------------------------------------------------------
 /// INCLUDES
-///-------------------------------------
-// NONE
-///-------------------------------------
+///----------------------------------------------------------------------------
+#include <Ethernet.h>
+///----------------------------------------------------------------------------
 /// CLASS VARIABLES
-///-------------------------------------
-// NONE
-///-------------------------------------
+///----------------------------------------------------------------------------
+const byte ETHERNET_SHIELD_MAC_ADDRESS[] =
+                        {0x90, 0xA2, 0xDA, 0x00, 0x23, 0x13}; // MAC Address
+const byte ETHERNET_SHIELD_IP_ADDRESS[] = {192, 168, 1, 3}; // IPv4 Address 
+//-----------------------------------------------------------------------------
 /// CLASS DEFINITION
-///-------------------------------------
-----------------------
+///----------------------------------------------------------------------------
 class Arduino
 {
+  public:
   /**************************************************************************//**
-  * METHOD: Arduino()
+  * CONSTRUCTOR: Arduino::Arduino()
   *   Class default constructor.
-  * 
+  *
+  * @param[out] None
+  * @param[in]  None
   ******************************************************************************/
   Arduino();
-} // End class Arduino
+
+  /**************************************************************************//**
+  * METHOD: Arduino::Arduino(byte mac [6],  byte ip [4])
+  *   Class ethernet style constructor.
+  * 
+  * @param[out] None
+  * @param[in] byte mac[6] (MAC address)
+  * @param[in] byte ip[4] (IP address [IEEE 802.11 IPv4])
+  ******************************************************************************/
+  Arduino(byte mac [6],  byte ip [4]);
+
+  /**************************************************************************//**
+  * POINTER COPY CONSTRUCTOR: Arduino::Arduino(const Arduino &rhs)
+  *   Class copy constructor.
+  *
+  * @param[out] None
+  * @param[in]  const Arduino rhs - Reference to the right hand side of the 
+  *                                 equation
+  ******************************************************************************/
+  Arduino(const Arduino &rhs);
+
+  /**************************************************************************//**
+  * ASSIGNMENT OPERATOR: Arduino::operator=Arduino(const Arduino rhs)
+  *   Class assignment operator.
+  *
+  * @param[out] Arduino lhs - Reference to the left hand side of the equation
+  * @param[in]  const Arduino rhs - Reference to the right hand side of the 
+  *                                 equation 
+  ******************************************************************************/
+  Arduino operator=(const Arduino &rhs);
+ 
+  /**************************************************************************//**
+  * DESTRUCTOR: Arduino::~Arduino()
+  *   Class default destructor.
+  *
+  * @param[out] None
+  * @param[in]  None
+  ******************************************************************************/
+  ~Arduino();
+
+  /**************************************************************************//**
+  * METHOD: Arduino::init_ethernet()
+  *   Default ether net mode initializer.
+  *
+  * @param[out] None
+  * @param[in]  None
+  ******************************************************************************/
+  void init_ethernet();
+
+  protected:
+  private:
+  // Bytes for the MAC address
+  byte m_mac [6];
+  // Bytes for the IP address (assumed to be IPv4)
+  byte m_ip [4];
+}; // End class Arduino
 
 #endif // _ARDUINO_H_ 
 
